3c4d77c8dd9f9bf05725c33369dce28d7de046cf
Vasilii Stakrotckii <inferno681@gmail.com>
2024-07-05 15:17:38 +0000

    mypy

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,4 @@
-from deepface import DeepFace
+from deepface.DeepFace import represent
 
 from app.constants import MANY_FACES, MODEL
 
@@ -7,8 +7,8 @@ class FaceVerification:
     def __init__(self, link: str) -> None:
         self.link = link
 
-    def represent(self) -> list[float]:
-        result = DeepFace.represent(img_path=self.link, model_name=MODEL)
+    def represent(self) -> dict[str, list[float]]:
+        result = represent(img_path=self.link, model_name=MODEL)
         if len(result) > 1:
             raise ValueError(MANY_FACES)
         return {self.link: result[0]["embedding"]}
