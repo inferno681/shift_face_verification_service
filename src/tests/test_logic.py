ea6531a1d6063083a8acc83d5f206e60e82260b6
Vasilii Stakrotckii <inferno681@gmail.com>
2024-07-12 23:07:00 +0000

    wps added

--- src/tests/test_logic.py
+++ src/tests/test_logic.py
@@ -1,19 +1,21 @@
 import pytest
 from deepface.DeepFace import represent
 
-from app.constants import *
+from app.constants import MANY_FACES_MESSAGE, MODEL
 from app.main import FaceVerification
 
 
 def test_vector_generation(one_face_link):
+    """Тест генерации вектора."""
     result = FaceVerification(one_face_link).represent()
     expected_result = {
-        one_face_link: represent(one_face_link, MODEL)[0]["embedding"]
+        one_face_link: represent(one_face_link, MODEL)[0]['embedding'],
     }
     assert result == expected_result
 
 
 def test_many_faces_error(many_faces_link):
+    """Тест исключения (несколько лиц на изображении)."""
     with pytest.raises((ValueError)) as excinfo:
         FaceVerification(many_faces_link).represent()
-    assert str(excinfo.value) == MANY_FACES
+    assert str(excinfo.value) == MANY_FACES_MESSAGE
