818198120a47e6649edc3d26c187beb8512b4c80
Vasilii Stakrotckii <inferno681@gmail.com>
2024-08-15 17:30:45 +0200

    fix

--- src/app/service/__init__.py
+++ src/app/service/__init__.py
@@ -1,3 +1,4 @@
-from app.service.consumer import consumer
 from app.service.exceptions import ManyFacesError
 from app.service.service import FaceVerification
+
+from app.service.consumer import consumer  # isort:skip
